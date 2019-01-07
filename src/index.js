d446028f54642920479795ae489a09d4dd8cd0f1
Phumrapee Limpianchop <git@rayriffy.com>
2019-01-07 14:57:47 +0700

    :sparkles: Now it can retrive latest lottery URL

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
-import axios from 'axios'
 import bodyParser from 'body-parser'
 import cheerio from 'cheerio'
 import express from 'express'
+import rp from 'request-promise'
 
 const server = express()
+const {PORT = 3000} = process.env
 
 server.get('/check', (req, res) => {
   let data = []
-  res.send(data)
+
+  // Get latest lottery URL
+  rp({
+    uri: `https://news.sanook.com/lotto/`,
+    transform: body => {
+      return cheerio.load(body)
+    },
+  }).then($ => {
+    let lottoUrl = $(
+      'body > div.section.section--highlight.highlight-horo > div > div > div > div.lotto-highlight > div > div.lotto-check__btn-group.flex-box > div > a',
+    ).attr('href')
+    // Read all data
+    rp({
+      uri: lottoUrl,
+      transform: body => {
+        return cheerio.load(body)
+      },
+    }).then($ => {
+      // TODO: Read all lottery data
+    })
+  })
 })
 
-server.listen(process.env.PORT).use(bodyParser.json())
+server
+  .use(bodyParser.json())
+  .listen(PORT, () => console.log(`App listening on port ${PORT}!`))
