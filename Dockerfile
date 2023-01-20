e3b6c6a0fd0e0ba942a3cc6da2a92ce2ca974a34
Phumrapee Limpianchop <git@rayriffy.com>
2023-01-21 07:42:20 +0900

    fix: enforce port env

--- Dockerfile
+++ Dockerfile
@@ -25,6 +25,7 @@ COPY src src
 # COPY tsconfig.json .
 
 ENV ENV production
+ENV PORT
 CMD ["./bun", "src/index.ts"]
 
 EXPOSE 3000
--- src/index.ts
+++ src/index.ts
@@ -168,7 +168,7 @@ const app = new Elysia()
       )
     }
   })
-  .listen(3000)
+  .listen(process.env.PORT ?? 3000)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
