2b32af72e3fb0eb6aef955dd5fa57d9cd7b08b3d
Phumrapee Limpianchop <git@rayriffy.com>
2023-01-22 00:43:39 +0900

    refactor: simplified response

--- src/index.ts
+++ src/index.ts
@@ -6,99 +6,49 @@ import { getLotto } from './functions/getLotto'
 
 const app = new Elysia()
   .use(cors())
-  .get(
-    '/',
-    () =>
-      new Response(
-        JSON.stringify({
-          status: 'success',
-          response:
-            'Please go to https://github.com/rayriffy/thai-lotto-api#api for API usage',
-        }),
-        {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      )
-  )
-  .get(
-    '/ping',
-    () =>
-      new Response(
-        JSON.stringify({
-          status: 'success',
-          response: 'pong',
-        }),
-        {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      )
-  )
-  .get('/list/:page', async ({ params: { page } }) => {
+  .get('/', () => ({
+    status: 'success',
+    response:
+      'Please go to https://github.com/rayriffy/thai-lotto-api#api for API usage',
+  }))
+  .get('/ping', () => ({
+    status: 'success',
+    response: 'pong',
+  }))
+  .get('/list/:page', async ({ params: { page }, set }) => {
     try {
       const targetPage = page ?? '1'
 
       if (!Number.isSafeInteger(Number(targetPage))) {
-        return new Response(
-          JSON.stringify({
-            status: 'crash',
-            response: 'invalid positive integer',
-          }),
-          {
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            status: 400,
-          }
-        )
+        set.status = 400
+        return {
+          status: 'crash',
+          response: 'invalid positive integer',
+        }
       } else {
         const lists = await getList(Number(targetPage))
 
-        return new Response(
-          JSON.stringify({
-            status: 'success',
-            response: lists,
-          }),
-          {
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          }
-        )
+        return {
+          status: 'success',
+          response: lists,
+        }
       }
     } catch (e) {
-      return new Response(
-        JSON.stringify({
-          status: 'crash',
-          response: 'api cannot fulfill your request at this time',
-        }),
-        {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          status: 500,
-        }
-      )
+      set.status = 500
+      return {
+        status: 'crash',
+        response: 'api cannot fulfill your request at this time',
+      }
     }
   })
-  .get('/lotto/:id', async ({ params: { id } }) => {
+  .get('/lotto/:id', async ({ params: { id }, set }) => {
     try {
       if (!Number.isSafeInteger(Number(id))) {
-        return new Response(
-          JSON.stringify({
-            status: 'crash',
-            response: 'invalid positive integer',
-          }),
-          {
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            status: 400,
-          }
-        )
+        set.status = 400
+        return {
+          status: 'crash',
+          response: 'invalid positive integer',
+        }
       } else {
         const lotto = await getLotto(id)
 
@@ -112,62 +62,34 @@ const app = new Elysia()
 
         // res.setHeader('Access-Control-Allow-Origin', '*')
 
-        return new Response(
-          JSON.stringify({
-            status: 'success',
-            response: lotto,
-          }),
-          {
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          }
-        )
+        return {
+          status: 'success',
+          response: lotto,
+        }
       }
     } catch (e) {
-      return new Response(
-        JSON.stringify({
-          status: 'crash',
-          response: 'api cannot fulfill your request at this time',
-        }),
-        {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          status: 500,
-        }
-      )
+      set.status = 500
+      return {
+        status: 'crash',
+        response: 'api cannot fulfill your request at this time',
+      }
     }
   })
-  .get('/latest', async () => {
+  .get('/latest', async ({ set }) => {
     try {
       const latestLottery = await getList(1).then(o => o[0])
       const lotto = await getLotto(latestLottery.id)
 
-      return new Response(
-        JSON.stringify({
-          status: 'success',
-          response: lotto,
-        }),
-        {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      )
+      return {
+        status: 'success',
+        response: lotto,
+      }
     } catch (e) {
-      return new Response(
-        JSON.stringify({
-          status: 'crash',
-          response: 'api cannot fulfill your request at this time',
-        }),
-        {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          status: 500,
-        }
-      )
+      set.status = 500
+      return {
+        status: 'crash',
+        response: 'api cannot fulfill your request at this time',
+      }
     }
   })
   .listen(process.env.PORT ?? 3000)
